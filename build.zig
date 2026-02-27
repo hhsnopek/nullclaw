0b430025005cbf546230dc660f17ed314e03a7ca
Igor Somov <donprusne@gmail.com>
2026-02-26 22:39:53 -0300

    release: v2026.2.26

--- build.zig
+++ build.zig
@@ -242,7 +242,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const is_wasi = target.result.os.tag == .wasi;
-    const app_version = b.option([]const u8, "version", "Version string embedded in the binary") orelse "2026.2.25";
+    const app_version = b.option([]const u8, "version", "Version string embedded in the binary") orelse "2026.2.26";
     const channels_raw = b.option(
         []const u8,
         "channels",
